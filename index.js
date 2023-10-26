42e1a9f9defed5c15092acec909072a133856a00
brueqlv <gundarens@gmail.com>
2023-10-26 16:31:20 +0300

    Fixed configuration

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,13 +7,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_fetch_1 = __importDefault(require("node-fetch"));
+import fetch from 'node-fetch';
 const app = () => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield (0, node_fetch_1.default)('https://api.coingecko.com/api/v3/exchange_rates');
+    const response = yield fetch('https://api.coingecko.com/api/v3/exchange_rates');
     const data = yield response.json();
     console.log(data);
 });
