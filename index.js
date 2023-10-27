578667de5caae9648e1aacfecbee7a67deed084f
brueqlv <gundarens@gmail.com>
2023-10-27 10:39:10 +0300

    Got it working

--- index.js
+++ index.js
@@ -8,9 +8,54 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import fetch from 'node-fetch';
-const app = () => __awaiter(void 0, void 0, void 0, function* () {
+import prompts from 'prompts';
+const fetchExchangeRates = () => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield fetch('https://api.coingecko.com/api/v3/exchange_rates');
-    const data = yield response.json();
-    console.log(data);
+    const data = (yield response.json());
+    const exchangeRates = [];
+    Object.entries(data.rates).forEach((exchangeRate) => {
+        exchangeRates.push({
+            symbol: exchangeRate[0],
+            name: exchangeRate[1].name,
+            value: exchangeRate[1].value
+        });
+    });
+    return exchangeRates;
+});
+const app = () => __awaiter(void 0, void 0, void 0, function* () {
+    const exchangeRates = yield fetchExchangeRates();
+    //console.log(exchangeRates);
+    const response = yield prompts([
+        {
+            type: 'select',
+            name: 'fromCurrency',
+            message: 'Select currency to convert from?',
+            choices: exchangeRates.map((exchangeRate) => {
+                return {
+                    title: exchangeRate.name,
+                    value: exchangeRate
+                };
+            })
+        },
+        {
+            type: 'number',
+            name: 'amount',
+            message: 'How much?'
+        },
+        {
+            type: 'select',
+            name: 'toCurrency',
+            message: 'Select currency to convert from?',
+            choices: exchangeRates.map((exchangeRate) => {
+                return {
+                    title: exchangeRate.name,
+                    value: exchangeRate
+                };
+            })
+        }
+    ]);
+    const btcValue = response.amount / response.fromCurrency.value;
+    const endCurrencyValue = response.toCurrency.value * btcValue;
+    console.log(`From (${response.fromCurrency.symbol}) with amount of ${response.amount} we got ${btcValue} BTC that is equal tp ${response.toCurrency.symbol} / with amount: ${endCurrencyValue}`);
 });
 app();
--- index.ts
+++ index.ts
@@ -1,12 +1,73 @@
 import fetch from 'node-fetch';
+import prompts from 'prompts';
+
+type ExchangeRate = {
+    name: string,
+    symbol:string,
+    value: number
+}
+
+type ExchangeRateResponse = {
+    rates: ExchangeRate[]
+}
+
+const fetchExchangeRates = async (): Promise<ExchangeRate[]> => {
+    const response = await fetch('https://api.coingecko.com/api/v3/exchange_rates');
+    const data = (await response.json()) as ExchangeRateResponse;
+
+    const exchangeRates: ExchangeRate[] = [];
+
+    Object.entries(data.rates).forEach((exchangeRate) => {
+        exchangeRates.push({
+            symbol: exchangeRate[0],
+            name: exchangeRate[1].name,
+            value: exchangeRate[1].value
+        })
+    });
+
+    return exchangeRates;
+}
 
 
 
 const app = async () => {
-    const response = await fetch('https://api.coingecko.com/api/v3/exchange_rates');
-const data = await response.json();
+    const exchangeRates: ExchangeRate[] = await fetchExchangeRates();
+    //console.log(exchangeRates);
 
-console.log(data);
+    const response = await prompts([
+        {
+            type: 'select',
+            name: 'fromCurrency',
+            message: 'Select currency to convert from?',
+            choices: exchangeRates.map((exchangeRate: ExchangeRate) => {
+                return {
+                    title: exchangeRate.name,
+                    value: exchangeRate
+                }
+            })
+        },
+        {
+            type: 'number',
+            name: 'amount',
+            message: 'How much?'
+        },
+        {
+            type: 'select',
+            name: 'toCurrency',
+            message: 'Select currency to convert from?',
+            choices: exchangeRates.map((exchangeRate: ExchangeRate) => {
+                return {
+                    title: exchangeRate.name,
+                    value: exchangeRate
+                }
+            })
+        }
+    ]);
+
+    const btcValue = response.amount / response.fromCurrency.value;
+    const endCurrencyValue = response.toCurrency.value * btcValue;
+
+    console.log(`From (${response.fromCurrency.symbol}) with amount of ${response.amount} we got ${btcValue} BTC that is equal tp ${response.toCurrency.symbol} / with amount: ${endCurrencyValue}`);
 }
 
 app();
